2f7937bad52307842a0f6689fb6dd9d9d7c49810
Bobby Johnson <bobby.johnson@auth0.com>
2019-05-05 17:13:34 -0700

    added vuex and vue-router and wired them up

---
+++ src/client/src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3 class="text-center">HomePage</h3>
+    <hr/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'homePage',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/client/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import ChatOverlayPage from '@/pages/ChatOverlayPage.vue';
+import HomePage from '@/pages/HomePage.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage,
+    },
+    {
+      path: '/overlays/chat',
+      name: 'ChatOverlayPage',
+      component: ChatOverlayPage,
+    },
+  ],
+});
