c877c48544222c5b5940fceaf612b5f6f40ad75a
Bobby Johnson <bobby.johnson@auth0.com>
2019-05-09 09:10:50 -0700

    fixing formatting

--- src/server/Handlers/Chat/ShoutOutHandler.cs
+++ src/server/Handlers/Chat/ShoutOutHandler.cs
@@ -12,9 +12,10 @@ namespace Bivrost.Web.Handlers.Chat
   public class ShoutOutHandler : INotificationHandler<RecievedChatMessageNotification>
   {
     public TwitchClient Client { get; }
+
     public ILogger<ShoutOutHandler> Logger { get; }
 
-  public Regex CommandRegex { get; } = new Regex(@"^!so (\w*)\s*?$");
+    public Regex CommandRegex { get; } = new Regex(@"^!so (\w*)\s*?$");
 
     public ShoutOutHandler(TwitchClient client, ILogger<ShoutOutHandler> logger)
     {
@@ -24,11 +25,12 @@ namespace Bivrost.Web.Handlers.Chat
 
     public Task Handle(RecievedChatMessageNotification notification, CancellationToken cancellationToken)
     {
-      return Task.Run(() => {
-        if(Client.IsConnected && notification.Message.Message.StartsWith("!so"))
+      return Task.Run(() =>
+      {
+        if (Client.IsConnected && notification.Message.Message.StartsWith("!so"))
         {
           var match = CommandRegex.Match(notification.Message.Message);
-          if(match.Success)
+          if (match.Success)
           {
             Client.SendMessage(notification.Message.Channel,
             $"Please checkout our friend {match.Groups[1].Value}'s stream at https://www.twitch.tv/{match.Groups[1].Value}");
@@ -37,18 +39,21 @@ namespace Bivrost.Web.Handlers.Chat
           {
             Client.SendWhisper(notification.Message.Username,
               @"I am sorry, but I did not understand your shoutout command.");
-              Client.SendWhisper(notification.Message.Username,
-              @"Please use the format: !so username");
+            Client.SendWhisper(notification.Message.Username,
+            @"Please use the format: !so username");
           }
         }
-        else if(!Client.IsConnected)
+        else if (!Client.IsConnected)
         {
           Logger.LogWarning("{@Event}",
-          new { Event="Shout Out Failed",
-                Reason="Twitch Client Not Connected",
-                UserId= notification.Message.UserId,
-                DisplayName = notification.Message.DisplayName,
-                Message=notification.Message.Message });
+          new
+          {
+            Event = "Shout Out Failed",
+            Reason = "Twitch Client Not Connected",
+            UserId = notification.Message.UserId,
+            DisplayName = notification.Message.DisplayName,
+            Message = notification.Message.Message
+          });
         }
       });
 
