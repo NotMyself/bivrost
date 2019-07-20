d1308936bf195a322722d04f9f05e51b3a122497
Bobby Johnson <bobby.johnson@auth0.com>
2019-07-19 18:13:53 -0700

    added chat module specs

---
+++ src/client/tests/unit/store/modules/chat.spec.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai';
+import module from '@/store/modules/chat';
+
+describe('given a chat module', () => {
+  describe('when getters are consumed', () => {
+    it('should get display messages', () => {
+      const state = {
+        chatMessages: []
+      };
+
+      expect(module.getters.displayMessages(state)).to.be.empty;
+    });
+  });
+  describe('when mutations are executed', () => {
+    it('should add a message', () => {
+      const state = {
+        chatMessages: [],
+        limit: 5
+      };
+
+      module.mutations.ADD_MESSAGE(state, {});
+
+      expect(state.chatMessages.length).to.equal(1);
+    });
+    it('should remove the first message when limit is exceeded', () => {
+      const message1 = { id: 1 };
+      const message2 = { id: 2 };
+      const state = {
+        chatMessages: [message1],
+        limit: 1
+      };
+
+      module.mutations.ADD_MESSAGE(state, message2);
+
+      expect(state.chatMessages).to.not.contain(message1);
+      expect(state.chatMessages).to.contain(message2);
+    });
+    it('should delete a message', () => {
+      const message = { id: 1 };
+      const state = {
+        chatMessages: [message],
+        limit: 5
+      };
+
+      module.mutations.DELETE_MESSAGE(state, message);
+
+      expect(state.chatMessages).to.not.contain(message);
+    });
+  });
+});
