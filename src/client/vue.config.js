9701865e6d71aad513bf47e89e4bc664bec6e19b
Bobby Johnson <bobby.johnson@auth0.com>
2019-12-10 11:40:57 -0800

    removed HMR hack from vue.config.js

--- src/client/vue.config.js
+++ src/client/vue.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-  configureWebpack: function(config) {
-    let plugins = [];
-    for (let i = 0; i < config.plugins.length; i++) {
-      if (config.plugins[i].constructor.name !== 'HotModuleReplacementPlugin') {
-        plugins.push(config.plugins[i]);
-      }
-    }
-
-    config.plugins = plugins;
-  },
-
   css: {
     sourceMap: true
   }
